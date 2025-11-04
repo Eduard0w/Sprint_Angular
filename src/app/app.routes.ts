3a96ff7f1d155c99b6a182e61887459efcaa886e
Eduardo Brito <dudusouza5595@gmail.com>
2025-11-04 16:29:51 -0300

    login e home foram quase completos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'Home', component: HomeComponent},
+    { path: 'Dashboard', component: DashboardComponent},
+];
+
+// @NgModule serve para agrupar componentes, serviços e confgurações que pertencem a uma mesma parte da aplicação.
+@NgModule({
+    //O RouterModule gerencia as rotas, o forRoot recebe uma lista de rotas que você definiu (em Routes (linha: 8))
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
